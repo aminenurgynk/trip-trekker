e8fc2971762744bedc14e53d17236bd9b3db9ef4
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-21 11:51:15 +0300

    feat: give title, description. add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
+// app/page.tsx
 
 export default function Home() {
   return (
-    <h1 className="text-bold text-teal-500">Hello World!</h1>
+    <h1 className="font-bold text-teal-500 text-2xl">Hello World!</h1>
   )
 }
