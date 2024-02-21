886f3eee954833578b48fde08b720b099c4c3652
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-21 13:13:49 +0300

    feat: navbar links almost done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,34 @@
-import Image from "next/image"
-import Link from "next/link"
-
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/trip-trekker-logo.svg" width={150} height={100} alt="logo" />
-        <ul className="hidden h-full gap-12 lg:flex">
-        </ul>
+        <Image
+          src="/trip-trekker-logo.svg"
+          width={150}
+          height={100}
+          alt="logo"
+        />
       </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray- flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <button>Login</button>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
