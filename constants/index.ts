62f93980b9672e5c8a6db6e2e28af2a38e867001
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-21 16:58:25 +0300

    feat: second section (camp) done

--- constants/index.ts
+++ constants/index.ts
@@ -70,7 +70,7 @@ export const NAV_LINKS = [
     title: 'Contact Us',
     links: [
       { label: 'Admin Officer', value: '123-456-7890' },
-      { label: 'Email Officer', value: 'amine.gynk@gmail.com' },
+      { label: 'Email Officer', value: 'trip.trekk@gmail.com' },
     ],
   };
   
