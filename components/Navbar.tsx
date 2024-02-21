4006fc68f948e371e3d1832c5b4b45a753a38768
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-21 13:00:09 +0300

    docs: adding some image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/trip-trekker-logo.svg" width={150} height={100} alt="logo" />
+        <ul className="hidden h-full"></ul>
+      </Link>
+    </nav>
   )
 }
 
