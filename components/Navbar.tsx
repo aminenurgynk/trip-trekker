bc9a2f658ddf19f809a8ffb4df7edd21f9e92914
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-21 13:36:28 +0300

    feat: login button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,7 +26,12 @@ const Navbar = () => {
         ))}
       </ul>
       <div className="lg:flexCenter hidden">
-        <Button />
+        <Button
+        type="button"
+        title="Login"
+        icon="/user.svg"
+        variant="btn_dark_green" 
+        />
       </div>
     </nav>
   );
