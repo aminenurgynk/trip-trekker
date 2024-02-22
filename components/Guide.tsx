8b0aa96ee1f0e496f55e8ebde5d3899dc31a5299
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-22 11:40:03 +0300

    feat: Guide part done

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Guide = () => {
   return (
     <section className='flexCenter flex-col'>
-      <div className='border-2 border-red-500 padding-container max-container w-full pb-24'>
+      <div className=' padding-container max-container w-full pb-24'>
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
         <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>We are here for you</p>
         <div className='flex flex-wrap justfiy-between gap-5 lg:gap-10'>
@@ -14,7 +14,7 @@ const Guide = () => {
 </p>
         </div>
       </div>
-      <div className='flexCenter max-container relative w-full border-2 border-blue-500'>
+      <div className='flexCenter max-container relative w-full'>
         <Image 
         src="/boat.png"
         alt='boat'
@@ -23,6 +23,7 @@ const Guide = () => {
         className='w-full object-cover object-center 2xl:rounded-5xl'
         />
         <div className='absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20'>
+          <Image src="/meter.svg" alt="meter" width={16} height={158} className='h-full w-auto'/>
           <div className='flexBetween flex-col'>
 
             <div className='flex w-full flex-col'>
@@ -34,11 +35,8 @@ const Guide = () => {
             </div>
             
             <div className='flex w-full flex-col'>
-              <div className='flexBetween w-full'>
-                <p className='regular-16 text-gray-20'>Destination</p>
-                <p className='bold-16 text-green-50'>48min</p>
-              </div>
-              <p className='bold-20 mt-2'>Demre</p>
+                <p className='regular-16 text-gray-20'>Start track</p>
+              <h4 className='bold-20 mt-2 whitespace-nowrap'>Andriake</h4>
             </div>
             
           </div>
